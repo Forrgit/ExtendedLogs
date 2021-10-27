ebb960c15f542861bd4c94f38196d6e16b36167b
AlexanderShumeyko <Forrgit@yandex.ru>
2021-10-28 02:07:53 +0300

    Add bp file info to log

--- Plugins/ExtendedLogs/Source/ExtendedLogs/Private/ELBlueprintFunctionLibrary.cpp
+++ Plugins/ExtendedLogs/Source/ExtendedLogs/Private/ELBlueprintFunctionLibrary.cpp
@@ -1,6 +1,19 @@
 #include "ELBlueprintFunctionLibrary.h"
 
+#include "Runtime/Launch/Resources/Version.h"
+
 void UELBlueprintFunctionLibrary::Log(const FString& Message, FELLogCategory LogCategory, EELLogVerbosity LogVerbosity)
 {
-	FMsg::Logf(nullptr, 0, LogCategory.Name, static_cast<ELogVerbosity::Type>(LogVerbosity), TEXT("%s"), *Message);
+	FString bpFile;
+
+#if (ENGINE_MAJOR_VERSION == 4 && ENGINE_MINOR_VERSION >= 26) || ENGINE_MAJOR_VERSION > 4
+	const FBlueprintContextTracker* blueprintExceptionTracker = FBlueprintContextTracker::TryGet();
+	if (blueprintExceptionTracker != nullptr && blueprintExceptionTracker->GetScriptStack().Num() > 0)
+	{
+		const FFrame* lastFrame = blueprintExceptionTracker->GetScriptStack().Last();
+		bpFile = GetNameSafe(lastFrame->Object);
+	}
+#endif
+
+	FMsg::Logf(nullptr, 0, LogCategory.Name, static_cast<ELogVerbosity::Type>(LogVerbosity), TEXT("%s [%s]"), *Message, *bpFile);
 }
