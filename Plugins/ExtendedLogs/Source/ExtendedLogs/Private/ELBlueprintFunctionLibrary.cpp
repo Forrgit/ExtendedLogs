5a0fbd9cb6417b4eccc209e10c7e831097ef21cb
AlexanderShumeyko <Forrgit@yandex.ru>
2021-11-02 02:57:25 +0300

    Added print string to log function

--- Plugins/ExtendedLogs/Source/ExtendedLogs/Private/ELBlueprintFunctionLibrary.cpp
+++ Plugins/ExtendedLogs/Source/ExtendedLogs/Private/ELBlueprintFunctionLibrary.cpp
@@ -4,31 +4,42 @@
 #include "ELLogManager.h"
 
 #include "ExtendedLogs.h"
+#include "Kismet/KismetSystemLibrary.h"
 #include "Runtime/Launch/Resources/Version.h"
 
-void UELBlueprintFunctionLibrary::Log(const FString& Message, FELLogCategoryName LogCategoryName, EELLogVerbosity LogVerbosity)
+void UELBlueprintFunctionLibrary::Log(const FString& Message, FELLogCategoryName LogCategoryName, EELLogVerbosity LogVerbosity, const UObject* WorldContextObject, bool bPrintToScreen, FLinearColor ScreenTextColor, float ScreenDuration)
 {
 	const auto nativeLogVerbosity = ConvertLogCategory(LogVerbosity);
 
+	const auto settings = UELExtendedLogsSettings::Get();
 	const auto logManager = FExtendedLogsModule::GetLogManager();
 
 	if (logManager == nullptr
 	    || (LogCategoryName.IsValid() && logManager->IsSuppressedLogCategory(LogCategoryName.Name, nativeLogVerbosity))
-	    || (!UELExtendedLogsSettings::Get()->bAllowBPLogsOutputToEmptyOrInvalidCategories && logManager->FindLogCategory(LogCategoryName.Name).Num() == 0))
+	    || (!settings->bAllowBPLogsOutputToEmptyOrInvalidCategories && logManager->FindLogCategory(LogCategoryName.Name).Num() == 0))
 	{
 		return;
 	}
 
-	FString logSuffix;
+	FString messageSuffix;
 
 #if (ENGINE_MAJOR_VERSION == 4 && ENGINE_MINOR_VERSION >= 26) || ENGINE_MAJOR_VERSION > 4
 	const FBlueprintContextTracker* blueprintExceptionTracker = FBlueprintContextTracker::TryGet();
 	if (blueprintExceptionTracker != nullptr && blueprintExceptionTracker->GetScriptStack().Num() > 0)
 	{
 		const FFrame* lastFrame = blueprintExceptionTracker->GetScriptStack().Last();
-		logSuffix = lastFrame->GetStackDescription();
+		messageSuffix = lastFrame->GetStackDescription();
 	}
 #endif
 
-	FMsg::Logf(nullptr, 0, LogCategoryName.Name, nativeLogVerbosity, TEXT("%s [%s]"), *Message, *logSuffix);
+	FMsg::Logf(nullptr, 0, LogCategoryName.Name, nativeLogVerbosity, TEXT("%s [%s]"), *Message, *messageSuffix);
+
+	if (bPrintToScreen)
+	{
+		const auto printToScreenLogData = settings->PrintLogsToScreenVerbosityMap.Find(LogVerbosity);
+		if (printToScreenLogData == nullptr || !printToScreenLogData->bPrintToScreen)
+		{
+			UKismetSystemLibrary::PrintString(WorldContextObject, FString::Printf(TEXT("%s [%s]"), *Message, *messageSuffix), true, false, ScreenTextColor, ScreenDuration);
+		}
+	}
 }
--- Plugins/ExtendedLogs/Source/ExtendedLogs/Public/ELBlueprintFunctionLibrary.h
+++ Plugins/ExtendedLogs/Source/ExtendedLogs/Public/ELBlueprintFunctionLibrary.h
@@ -11,7 +11,6 @@ class EXTENDEDLOGS_API UELBlueprintFunctionLibrary : public UBlueprintFunctionLi
 {
 	GENERATED_BODY()
 public:
-	//UFUNCTION(BlueprintCallable, Category = "Extended logs", meta = (DisplayName = "UE LOG"))
-	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
-	static void Log(const FString& Message, FELLogCategoryName LogCategoryName, EELLogVerbosity LogVerbosity = EELLogVerbosity::Log);
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", WorldContext = "WorldContextObject", CallableWithoutWorldContext, Keywords = "log print", AdvancedDisplay = "3"))
+	static void Log(const FString& Message, FELLogCategoryName LogCategoryName, EELLogVerbosity LogVerbosity = EELLogVerbosity::Log, const UObject* WorldContextObject = nullptr, bool bPrintToScreen = false, FLinearColor ScreenTextColor = FLinearColor(0.0, 0.66, 1.0), float ScreenDuration = 2.f);
 };
